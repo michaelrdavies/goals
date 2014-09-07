5a9382f8f4ff7babd9846c54bafe26cc2a04d74a
michaelrdavies <davies.mike@gmail.com>
2014-09-07 11:54:57 -0700

    Update layout of pages and futzed around with some jQuery Mobile stuff but left it commented out.

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,9 +1,9 @@
 class Goal < ActiveRecord::Base
   @@values = Hash.new()
   @@values[0] = "worthless"
-  @@values[1] = "insignificant"
+  #@@values[1] = "insignificant"
   @@values[2] = "worthwhile"
-  @@values[3] = "valuable"
+  #@@values[3] = "valuable"
   @@values[4] = "priceless"
 
   def self.values
@@ -12,9 +12,9 @@ class Goal < ActiveRecord::Base
 
   @@efforts = Hash.new()
   @@efforts[0] = "trivial"
-  @@efforts[1] = "easy"
+  #@@efforts[1] = "easy"
   @@efforts[2] = "doable"
-  @@efforts[3] = "challenging"
+  #@@efforts[3] = "challenging"
   @@efforts[4] = "grueling"
 
   def self.efforts
@@ -23,14 +23,14 @@ class Goal < ActiveRecord::Base
 
   @@days = Hash.new()
   @@days[1] = "a day"
-  @@days[7] = "a week"
+  #@@days[7] = "a week"
   @@days[30] = "a month"
-  @@days[183] = "6 months"
+  #@@days[183] = "6 months"
   @@days[365] = "a year"
-  @@days[1825] = "5 years"
-  @@days[3650] = "10 years"
-  @@days[10950] = "a career"    #TODO 30 years. Depends on age.
-  @@days[18250] = "a lifetime"  #TODO 50 years. Depends on age.
+  #@@days[1825] = "5 years"
+  #@@days[3650] = "10 years"
+  #@@days[10950] = "a career"    #TODO 30 years. Depends on age.
+  #@@days[18250] = "a lifetime"  #TODO 50 years. Depends on age.
 
   def self.days
     @@days
